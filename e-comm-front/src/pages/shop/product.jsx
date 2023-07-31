f41c5560a63557830ce40fffb9615905a8779495
Mickael <mickael.lesueur@epitech.eu>
2023-07-31 22:13:32 +0200

    ptite mise en place du pannier

--- e-comm-front/src/pages/shop/product.jsx
+++ e-comm-front/src/pages/shop/product.jsx
@@ -1,7 +1,8 @@
-import React from 'react'
-
+import React, { useContext } from 'react'
+import { ShopContext } from '../../context/shop-context';
 export const Product = (props) => {
-    const {id, productName, price, productImage} =props.data;
+    const {id, productName, price, productImage} = props.data;
+    const { addToCart } = useContext(ShopContext);
   return (
     <div className='product'>
         <img src={productImage} alt={productName}/>
@@ -9,6 +10,7 @@ export const Product = (props) => {
             <p><b>{productName}</b></p>
             <p>${price}</p>
         </div>
+        <button className='addToCartBttn' onClick={() => addToCart(id)}> Add To Cart</button>
         </div>
   )
 }
