6351508d3da6c3849ae58200bbdf97b2699e0111
aimeemussard <aimee.mussard@epitech.eu>
2023-08-01 12:24:47 +0200

    [ADD] form front && handlesubmit

--- e-comm-front/src/App.js
+++ e-comm-front/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Navbar } from "./components/navbar";
 import { Shop } from "./pages/shop/shop"
 import { Cart } from "./pages/cart/cart";
+import { Register } from "./pages/user/register";
 function App() {
   return (
     <div className="App">
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Shop />}/>
           <Route path="/Pannier" element={<Cart />}/>
+          <Route path="/register" element={<Register />}/>
 
         </Routes>
       </Router>
---
+++ e-comm-front/src/pages/user/register.jsx
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+export class Register extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      user: {
+        email: '',
+        password: '',
+      },
+    };
+
+    this.handleEmailChange = this.handleEmailChange.bind(this);
+    this.handlePasswordChange = this.handlePasswordChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleEmailChange(event) {
+    const user = { ...this.state.user, email: event.target.value };
+    this.setState({ user });
+  }
+
+  handlePasswordChange(event) {
+    const user = { ...this.state.user, password: event.target.value };
+    this.setState({ user });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    
+    alert(this.state.user);
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Email:
+          <input type="text" value={this.state.user.email} onChange={this.handleEmailChange} />
+        </label>
+        <label>
+          Password:
+          <input type="password" value={this.state.user.password} onChange={this.handlePasswordChange} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
+
+export default Register;
