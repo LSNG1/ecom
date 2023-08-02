045e393e71ae00ab9d0fd0c703271af0b3659050
aimeemussard <aimee.mussard@epitech.eu>
2023-08-02 10:55:25 +0200

    [ADD] Slider for country / region + object user updated

--- e-comm-front/src/pages/user/register.jsx
+++ e-comm-front/src/pages/user/register.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { CountryDropdown, RegionDropdown, CountryRegionData } from 'react-country-region-selector';
+
 import axios from 'axios';
 
 export class Register extends Component {
@@ -12,6 +14,7 @@ export class Register extends Component {
         name: '',
         lastname: '',
         country: '',
+        region: '',
         birthdate: ''
       },
     };
@@ -22,16 +25,12 @@ export class Register extends Component {
     this.handleLastNameChange = this.handleLastNameChange.bind(this);
     this.handleCountryChange = this.handleCountryChange.bind(this);
     this.handleBirthdateChange = this.handleBirthdateChange.bind(this);
-
-
-
-
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleRegionChange = this.handleRegionChange.bind(this);
+
   }
 
   handleEmailChange(event) {
-
-
     const user = { ...this.state.user, email: event.target.value };
     this.setState({ user });
   }
@@ -50,29 +49,62 @@ export class Register extends Component {
     const user = { ...this.state.user, lastname: event.target.value };
     this.setState({ user });
   }
-
-  handleCountryChange(event) {
-    const user = { ...this.state.user, country: event.target.value };
-    this.setState({ user });
-  }
-
+  
+  
   handleBirthdateChange(event) {
     const user = { ...this.state.user, birthdate: event.target.value };
     this.setState({ user });
   }
+  
+  handleCountryChange(val) {
+    const user = { ...this.state.user, country: val };
+    this.setState({ user });
+  }
 
+  handleRegionChange(val) {
+    const user = { ...this.state.user, region: val };
+    this.setState({ user });
+  }
+
+  
+
+  
+  // selectCountry (val) {
+  //   this.setState({ country: val });
+  // }
+
+  // selectRegion (val) {
+  //   this.setState({ region: val });
+  // }
 
   handleSubmit(event) {
     event.preventDefault();
+	
 
-    let regex = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
+    let regex_email = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
+    let regex_country = /^[A-Za-z]+$/;
+    let regex_name = /[A-Za-z]/;
 
-    if (!regex.test(this.state.user.email)) {
+    if (!regex_email.test(this.state.user.email)) {
       alert("Invalid email address!");
     } else {
       alert("Valid email address!");
     }
-    
+    if(!(regex_country.test(this.state.user.country)) && this.state.user.country.length < 3) {
+    	alert("Invalid country")
+    } else {
+    	alert("Country valid")
+    }
+    if(!regex_name.test(this.state.user.name)) {
+    	alert("invalid name format")
+    } else {
+    	alert("valid name")
+    }
+    if(!regex_name.test(this.state.user.lastname)) {
+    	alert("invalid lastname format")
+    } else {
+    	alert("valid lastname")
+    }
     alert(this.state.user.birthdate);
   }
 
@@ -95,10 +127,19 @@ export class Register extends Component {
           Prénom:
         <input type="text" value={this.state.user.lastname} onChange={this.handleLastNameChange} />
         </label>
-        <label>
+        <CountryDropdown
+          value={this.state.user.country}
+          onChange={(val) => this.handleCountryChange(val)} />
+
+        <RegionDropdown
+          country={this.state.user.country}
+          value={this.state.user.region}
+          onChange={(val) => this.handleRegionChange(val)} />
+      
+        {/* <label>
           Pays:
         <input type="text" value={this.state.user.country} onChange={this.handleCountryChange} />
-        </label>
+        </label> */}
         <label>
           Date de naissance:
         <input type="date" value={this.state.user.birthdate} onChange={this.handleBirthdateChange} />
