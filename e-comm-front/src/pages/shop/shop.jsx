2e623168207b9039d857cafbdb875b32d530d9c7
Mickael <mickael.lesueur@epitech.eu>
2023-08-02 16:59:37 +0200

    un peu de style pour qu'on kiff bien

--- e-comm-front/src/pages/shop/shop.jsx
+++ e-comm-front/src/pages/shop/shop.jsx
@@ -5,16 +5,39 @@ import './shop.css';
 import SwipeableTextMobileStepper from '../../components/carousel'
 
 export const Shop = () => {
+  // Get the first product from the PRODUCTS array
+  const firstProduct = PRODUCTS[0];
+  const secondProduct = PRODUCTS[1];
+  const thirdProduct = PRODUCTS[2];
+  const fourthProduct = PRODUCTS[3];
+  const fifthProduct = PRODUCTS[4];
+  const sixedProduct = PRODUCTS[5];
+  const seventhProduct = PRODUCTS[6];
+  const eightProduct = PRODUCTS[7];
+  const ninthProduct = PRODUCTS[8];
+
   return (
     <div className='shop'>
-      <SwipeableTextMobileStepper />
+      <div className='Carousel'>
+        <SwipeableTextMobileStepper />
+      </div>
       <div className='shopTitle'>
         <h1>Welcome Page</h1>
       </div>
       <div className='products'>
-        {PRODUCTS.map((product) => (
-          <Product data={product} />
-        ))}
+        <Product data={firstProduct} />
+        <Product data={secondProduct} />
+        <Product data={thirdProduct} />
+      </div>
+      <div className='products'>
+        <Product data={fourthProduct} />
+        <Product data={fifthProduct} />
+        <Product data={sixedProduct} />
+      </div>
+      <div className='products'>
+        <Product data={seventhProduct} />
+        <Product data={eightProduct} />
+        <Product data={ninthProduct} />
       </div>
     </div>
   );
--- e-comm-front/src/products.js
+++ e-comm-front/src/products.js
@@ -1,11 +1,12 @@
 import product1 from './assets/pngimg.com - iphone_13_PNG2.png'
-import product2 from './assets/pngimg.com - iphone_13_PNG2.png'
-import product3 from './assets/pngimg.com - iphone_13_PNG2.png'
-import product4 from './assets/pngimg.com - iphone_13_PNG2.png'
-import product5 from './assets/pngimg.com - iphone_13_PNG2.png'
-import product6 from './assets/pngimg.com - iphone_13_PNG2.png'
-import product7 from './assets/pngimg.com - iphone_13_PNG2.png'
-import product8 from './assets/pngimg.com - iphone_13_PNG2.png'
+import product2 from './assets/7d204cd60ff1fce24cf6d11f24cd210a.png'
+import product3 from './assets/carte-mere-pc-gamer.png'
+import product4 from './assets/g713-gallery-1.webp'
+import product5 from './assets/geforce-rtx-2080-technical-photography-angled-003.png'
+import product6 from './assets/logo.png'
+import product7 from './assets/RE1uwh1.png'
+import product8 from './assets/vengeance_rgb_pro_sl_black_1_500x500.png'
+import product9 from './assets/F2EGCfrX0AIv3yt.jpg'
 
 export const PRODUCTS = [
     {
@@ -55,5 +56,11 @@ export const PRODUCTS = [
         productName: "IPhone",
         price: 999,
         productImage: product8,
-    }
+    },
+    {
+        id:9,
+        productName: "IPhone",
+        price: 999,
+        productImage: product9,
+    },
 ]
