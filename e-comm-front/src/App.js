fc4302f619c094ea4adac354d324ad3978acb810
LSNG <lsng@protonmail.com>
2023-08-08 04:50:45 +0200

    admin API/platform full

--- e-comm-front/src/App.js
+++ e-comm-front/src/App.js
@@ -1,24 +1,26 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Navbar } from "./components/navbar";
-import { Shop } from "./pages/shop/shop"
+import { Shop } from "./pages/shop/shop";
 import { Cart } from "./pages/cart/cart";
 import { ShopContextProvider } from "./context/shop-context";
 import { Register } from "./pages/user/register";
+import { Admin } from "./pages/admin/admin";
+
 function App() {
-	
   return (
     <div className="App">
       <ShopContextProvider>
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Shop />}/>
-          <Route path="/Cart" element={<Cart />}/>
-          <Route path="/register" element={<Register />}/>
-
-        </Routes>
-      </Router>
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Shop />} />
+            <Route path="/Cart" element={<Cart />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/admin/*" element={<Admin />}>
+            </Route>
+          </Routes>
+        </Router>
       </ShopContextProvider>
     </div>
   );
