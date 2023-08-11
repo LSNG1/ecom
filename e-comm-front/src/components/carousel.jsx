ca038d7ca28e5edde5b6240459ea5fea35b23add
Mickael <mickael.lesueur@epitech.eu>
2023-08-11 15:33:15 +0200

    le front est beau de fou

--- e-comm-front/src/components/carousel.jsx
+++ e-comm-front/src/components/carousel.jsx
@@ -1,12 +1,6 @@
 import * as React from 'react';
 import { useTheme } from '@mui/material/styles';
 import Box from '@mui/material/Box';
-import MobileStepper from '@mui/material/MobileStepper';
-import Paper from '@mui/material/Paper';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
-import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
 import SwipeableViews from 'react-swipeable-views';
 import { autoPlay } from 'react-swipeable-views-utils';
 import "./carousel.css";
@@ -16,50 +10,22 @@ const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
 
 const images = [
     {
-<<<<<<< HEAD
-=======
-        label: "",
->>>>>>> refs/remotes/origin/visuel
         imgPath:
             'https://media.topachat.com/media/s1000/649be289cb42ac1f062be67d.webp',
     },
     {
-<<<<<<< HEAD
-=======
-        label: "",
->>>>>>> refs/remotes/origin/visuel
         imgPath:
             'https://media.topachat.com/media/s1000/64c77d9e642f572b251f3ddf.webp',
     },
     {
-<<<<<<< HEAD
-=======
-        label: "",
->>>>>>> refs/remotes/origin/visuel
         imgPath:
             'https://media.ldlc.com/encart/p/21462_b.jpg',
     },
     {
-<<<<<<< HEAD
-=======
-        label: "",
->>>>>>> refs/remotes/origin/visuel
-        imgPath:
-            'https://media.ldlc.com/encart/p/21501_b.jpg',
-    },
-    {
-<<<<<<< HEAD
-=======
-        label: "",
->>>>>>> refs/remotes/origin/visuel
         imgPath:
             'https://www.aorus.com/image/banner/GOELITE-1690280290.jpg',
     },
     {
-<<<<<<< HEAD
-=======
-        label: "",
->>>>>>> refs/remotes/origin/visuel
         imgPath:
             'https://media.topachat.com/media/s1000/63639dcc3b1f41344e0c11a3.webp',
     },
@@ -69,89 +35,38 @@ function SwipeableTextMobileStepper() {
     const theme = useTheme();
     const [activeStep, setActiveStep] = React.useState(0);
 
-    const handleNext = () => {
-        setActiveStep((prevActiveStep) => prevActiveStep + 1);
-    };
-
-    const handleBack = () => {
-        setActiveStep((prevActiveStep) => prevActiveStep - 1);
-    };
-
     const handleStepChange = (step) => {
         setActiveStep(step);
     };
 
     return (
-        <Box sx={{ minWidth: 800, flexGrow: 1 }}>
-            <Paper
-                square
-                elevation={0}
-                sx={{
-                    marginright: 160,
-                    display: 'flex',
-                    alignItems: 'center',
-                    height: 200,
-                    width: 600,
-                    pl: 2,
-                    bgcolor: 'background.default',
-                }}
-            >
-                <Typography>
-                    {images[activeStep] && images[activeStep].label}
-                </Typography>
-            </Paper>
-            <AutoPlaySwipeableViews
-                axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
-                index={activeStep}
-                onChangeIndex={handleStepChange}
-                enableMouseEvents
-            >
-                {images.map((step, index) => (
-                    <div key={step.label}>
-                        {Math.abs(activeStep - index) <= 2 ? (
-                            <Box
-                                component="img"
-                                sx={{
-                                    height: '1600',
-                                    display: 'block',
-                                    maxWidth: 3000,
-                                    overflow: 'hidden',
-                                    width: '400%%',
-                                }}
-                                src={step.imgPath}
-                                alt={step.label}
-                            />
-                        ) : null}
-                    </div>
-                ))}
+        <Box sx={{ maxWidth: 1000000000, flexGrow: 1 }}>
+            <div className="conteneur">
+        <AutoPlaySwipeableViews
+          axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
+          index={activeStep}
+          onChangeIndex={handleStepChange}
+          enableMouseEvents
+        >
+          {images.map((step, index) => (
+            <div key={step.label}>
+              {Math.abs(activeStep - index) <= 2 ? (
+                <Box
+                  component="img"
+                  sx={{
+                    height: 600,
+                    display: 'block',
+                    overflow: 'hidden',
+                    width: 1600,
+                  }}
+                  src={step.imgPath}
+                  alt={step.label}
+                />
+              ) : null}
+            </div>
+          ))}
             </AutoPlaySwipeableViews>
-            <MobileStepper
-                variant="progress"
-                steps={6}
-                position="static"
-                activeStep={activeStep}
-                sx={{ maxWidth: 400, flexGrow: 1 }}
-                nextButton={
-                    <Button size="small" onClick={handleNext} disabled={activeStep === 5}>
-                        Next
-                        {theme.direction === 'rtl' ? (
-                            <KeyboardArrowLeft />
-                        ) : (
-                            <KeyboardArrowRight />
-                        )}
-                    </Button>
-                }
-                backButton={
-                    <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
-                        {theme.direction === 'rtl' ? (
-                            <KeyboardArrowRight />
-                        ) : (
-                            <KeyboardArrowLeft />
-                        )}
-                        Back
-                    </Button>
-                }
-            />
+            </div>
         </Box>
     );
 }
--- e-comm-front/src/components/footer.jsx
+++ e-comm-front/src/components/footer.jsx
@@ -5,6 +5,9 @@ import Payment from "../assets/image.png"
 import { InstagramLogo } from "phosphor-react";
 import { TwitterLogo } from "phosphor-react";
 import { LinkedinLogo } from "phosphor-react";
+// // phosphore icons
+// import ReactDOM from "react-dom";
+// import { PaypalLogo } from "@phosphor-icons/react";
 import "./footer.css";
 
 const Footer = () => {
@@ -19,10 +22,12 @@ const Footer = () => {
         <div class="réseau"><InstagramLogo size={24} /></div>
         <div class="réseau"><TwitterLogo size={24} /></div>
         <div class="réseau"><LinkedinLogo size={24} /></div>
+        {/* <PaypalLogo size={32} weight="light" /> */}
+        
       </div>
 
     </footer>
   );
 };
-
+// ReactDOM.render(<Footer />, document.getElementById("root"));
 export default Footer;
